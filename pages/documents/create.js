9bfe28da264134f9a0512b05db348b30ced1909b
rglover <me@ryanglover.net>
2021-03-27 19:47:12 -0500

    fix ability to create blank documents

--- pages/documents/create.js
+++ pages/documents/create.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useRouter } from "next/router";
 import { useMutation } from "@apollo/client";
 import { createDocument as createDocumentMutation } from "../../graphql/mutations/Documents.gql";
+import ValidatedForm from "../../components/ValidatedForm";
 
 import { StyledCreateDocument } from "./styles";
 
@@ -11,9 +12,7 @@ const CreateDocument = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
+  const handleSubmit = () => {
     createDocument({
       variables: {
         document: {
@@ -38,40 +37,60 @@ const CreateDocument = () => {
 
   return (
     <StyledCreateDocument>
-      <form onSubmit={handleSubmit}>
-        <div className="row">
-          <div className="col-xs-12">
-            <div className="mb-3">
-              <label className="form-label">Document Title</label>
-              <input
-                className="form-control"
-                type="text"
-                name="title"
-                value={title}
-                onChange={(event) => setTitle(event.target.value)}
-                placeholder="Document Title"
-              />
+      <ValidatedForm
+        rules={{
+          title: {
+            required: true,
+          },
+          content: {
+            required: true,
+          },
+        }}
+        messages={{
+          title: {
+            required: "Title is required.",
+          },
+          content: {
+            required: "Content is required.",
+          },
+        }}
+        onSubmit={handleSubmit}
+      >
+        <form>
+          <div className="row">
+            <div className="col-xs-12">
+              <div className="mb-3">
+                <label className="form-label">Document Title</label>
+                <input
+                  className="form-control"
+                  type="text"
+                  name="title"
+                  value={title}
+                  onChange={(event) => setTitle(event.target.value)}
+                  placeholder="Document Title"
+                />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-xs-12">
-            <div className="mb-4">
-              <label className="form-label">Document Content</label>
-              <textarea
-                className="form-control"
-                name="content"
-                value={content}
-                onChange={(event) => setContent(event.target.value)}
-                placeholder="Document Content"
-              />
+          <div className="row">
+            <div className="col-xs-12">
+              <div className="mb-4">
+                <label className="form-label">Document Content</label>
+                <textarea
+                  className="form-control"
+                  name="content"
+                  value={content}
+                  onChange={(event) => setContent(event.target.value)}
+                  placeholder="Document Content"
+                />
+              </div>
             </div>
           </div>
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Create Document
-        </button>
-      </form>
+          <button type="submit" className="btn btn-primary">
+            Create Document
+          </button>
+        </form>
+      </ValidatedForm>
     </StyledCreateDocument>
   );
 };
